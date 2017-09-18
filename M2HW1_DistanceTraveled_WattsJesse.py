19706054d9fb034121443bf76fe7426a16ac6ddf
JesseWatts <31894507+JesseWatts@users.noreply.github.com>
2017-09-18 01:33:06 -0400

    Add files via upload
    Second submission

---
+++ M2HW1_DistanceTraveled_WattsJesse.py
@@ -0,0 +1,28 @@
+# CTI-110 
+# M2HW1-Distanced Traveled
+# Jesse Watts
+# 12 Sep 17
+# Get the distance traveled @ 70 mph in 6 hours, 10 hours, & 15 hours
+
+# Declare variables
+SPEED=70
+distance6Hours = SPEED * 6
+distance10Hours = SPEED * 10
+distance15Hours = SPEED * 15
+
+# Calculate the distance traveled in 6 hours if speed is 70 miles per hour
+distance=SPEED*distance6Hours
+distance=(70*6)
+
+# Calculate the distance traveled in 10 hours if speed is 70 miles per hour
+distance=SPEED*distance10Hours
+distance=(70*10)
+
+# Calculate the distance traveled in 15 hours if speed is 70 miles per hour.
+distance=SPEED*distance15Hours
+distance=(70*15)
+print('What is the distance traveled in 6 hours at 70 mph?',distance6Hours)
+print('What is the distance traveled in 10 hours at 70 mph?',distance10Hours)
+print('What is the distance traveled in 15 hours at 70 mph?',
+      distance15Hours)
+
